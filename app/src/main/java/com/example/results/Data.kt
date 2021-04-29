5d321f0e80ada26c994b12713403ba0c39168469
Gautam <devgautam1231@gmail.com>
2021-04-29 19:59:18 +0530

    done lot of updates, fixed bugs, added more functionality,

--- app/src/main/java/com/example/results/Data.kt
+++ app/src/main/java/com/example/results/Data.kt
@@ -11,6 +11,7 @@ package com.example.results
 
 //class to handle data to be set to list view as custom adapter
 data class Data(
+        //declare set of variables to be used
         val subject: String,
         val subjectCode: String,
         val internal: String,
